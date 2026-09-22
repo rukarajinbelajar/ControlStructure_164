12870d45d89f08566e939a1282d2e16493390fd1
rukarajinbelajar <maar1101dnd@gmail.com>
2026-09-22 10:44:39 +0700

    jika nilai input lebih dari 80 maka output yang muncul adalah "very good performance"

--- ControlStructure1.py
+++ ControlStructure1.py
@@ -1,4 +1,6 @@
-performance = float (input ("Enter Student Performance : "))
+performance = int (input ("Enter Student Performance : "))
 
 if performance >= 90:
     print ("Exellent Performance")
+elif performance >= 80:
+    print ("Very Good Performance")
