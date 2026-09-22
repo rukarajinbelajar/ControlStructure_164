64038eae09f6fb1eb46e99da9ab3c0c836a89d71
rukarajinbelajar <maar1101dnd@gmail.com>
2026-09-22 11:17:47 +0700

    membenarkan

--- ControlStucture2.py
+++ ControlStucture2.py
@@ -2,13 +2,13 @@ a = int (input("Masukkan Angka pertama : "))
 b = int (input("Masukkan Angka kedua: "))
 c = int (input("Masukkan Angka ketiga: "))
 
-if a >= b and a >= c :
+if a > b and a > c :
     largest = a
     print ("Angka terbesar adalah : ", largest)
-elif b >= a and b >= c :
+elif b > a and b > c :
     largest = b
     print ("Angka terbesar adalah : ", largest)
-elif c >= a and c>=b:
+elif c > a and c>b:
     largest = c
     print ("Angka terbesar adalah : ", largest)
 else:
