0852e742d4d92f0ea1e385e099d2c6001fff85a2
rukarajinbelajar <maar1101dnd@gmail.com>
2026-09-22 11:01:12 +0700

    jika nilai 1 lebih mesar, maka output yang akan keluar

--- ControlStucture2.py
+++ ControlStucture2.py
@@ -1,3 +1,6 @@
-a = int (input("Masukkan Angka pertama : "))
-b = int (input("Masukkan Angka kedua: "))
-c = int (input("Masukkan Angka ketiga: "))
+1 = int (input("Masukkan Angka pertama : "))
+2 = int (input("Masukkan Angka kedua: "))
+3 = int (input("Masukkan Angka ketiga: "))
+
+if 1 >= 2 and 1 >= 3 :
+    terbesar = 1
